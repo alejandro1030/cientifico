a995cc01fabb80c32c0cd98b8dec233251fd0e14
Jorge Norbey Alejandro Gonzalez Montealegre <jorgenalejandrogm@gmail.com>
2020-08-22 19:08:47 -0500

    Borré una coma al final

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
         ),
         new CopyWebpackPlugin({
             patterns: [{ from: './src/styles/styles.css',
-            to: '' }],
+            to: '' }]
         })
     ]
 }
